33068679e6ca61329938e4ead6184fd061da6164
JordanRondon <jordanangelrondonpozo@gmail.com>
2024-05-23 01:34:06 -0500

    cambio de fragmento dentro en una actividad

--- app/src/main/java/com/example/finanzaspersonales/HistorialActivity.kt
+++ app/src/main/java/com/example/finanzaspersonales/HistorialActivity.kt
@@ -5,8 +5,12 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.button.MaterialButtonToggleGroup
 
 class HistorialActivity : AppCompatActivity() {
+
+    private lateinit var toggleGroup: MaterialButtonToggleGroup
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -15,6 +19,27 @@ class HistorialActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        toggleGroup = findViewById(R.id.toggleButton)
+
+        toggleGroup.addOnButtonCheckedListener { group, checkedId, isChecked ->
+            if (isChecked) {
+                val fragment = when (checkedId) {
+                    R.id.btnGastos -> GastosFragment()
+                    R.id.BtnGraficos -> GraficosFragment()
+                    else -> null
+                }
+                fragment?.let {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.fragment_container, it)
+                        .commit()
+                }
+            }
+        }
+
+        if (savedInstanceState == null) {
+            toggleGroup.check(R.id.btnGastos)
         }
     }
 }
