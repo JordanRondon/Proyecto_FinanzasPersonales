e1631b7d895fe212bd4dc045a4fe27a8a2be88a7
Pablo AQ <alfaroquispepablo@gmail.com>
2024-06-08 18:59:48 -0500

    RecyclerView Categorias Complete

--- app/src/main/java/com/example/finanzaspersonales/Fragments/SheetGastos.kt
+++ app/src/main/java/com/example/finanzaspersonales/Fragments/SheetGastos.kt
@@ -1,87 +1,96 @@
 package com.example.finanzaspersonales.Fragments
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ArrayAdapter
 import android.widget.Toast
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.RecyclerView
 import com.example.finanzaspersonales.Clases.TaskViewModel
+import com.example.finanzaspersonales.R
+import com.example.finanzaspersonales.adaptadores.CategoriaGastosAdapter
 import com.example.finanzaspersonales.databinding.FragmentSheetGastosBinding
 import com.example.finanzaspersonales.entidades.Categoria
+import com.example.finanzaspersonales.entidades.CategoriaGastos
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseReference
+import androidx.recyclerview.widget.GridLayoutManager
 import com.google.firebase.database.FirebaseDatabase
-import java.time.LocalDateTime
 import java.time.ZoneId
 import java.time.ZonedDateTime
 import java.time.format.DateTimeFormatter
-import java.util.Calendar
 
 class SheetGastos : BottomSheetDialogFragment() {
 
     private lateinit var binding: FragmentSheetGastosBinding
     private lateinit var taskViewModel: TaskViewModel
-    private val arrayListCategoria: ArrayList<String> = ArrayList()
+    private val arrayListCategoria: ArrayList<CategoriaGastos> = ArrayList()
 
     private val zonedDateTime = ZonedDateTime.now(ZoneId.of("America/Lima"))
+    private val userId = FirebaseAuth.getInstance().currentUser?.uid
+    //private val database = FirebaseDatabase.getInstance().getReference("Categoria/$userId")
 
     private lateinit var database: DatabaseReference
+    private var idCategoria = 0
+
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var categoriaGastosAdapter: CategoriaGastosAdapter
+
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val activity = requireActivity()
         taskViewModel = ViewModelProvider(activity).get(TaskViewModel::class.java)
 
-        binding.btnGuardarCategoria.setOnClickListener {
-            saveCategoria()
-        }
-
-
+        recyclerView = view.findViewById(R.id.recyclerViewCategoriaGastos)
+        recyclerView.layoutManager = GridLayoutManager(requireContext(), 3)
+        categoriaGastosAdapter = CategoriaGastosAdapter(arrayListCategoria, requireContext())
+        recyclerView.adapter = categoriaGastosAdapter
 
 
         getCategorias()
+
+        binding.btnGuardarCategoria.setOnClickListener {
+            saveGastos()
+        }
+
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         binding = FragmentSheetGastosBinding.inflate(inflater, container, false)
         return binding.root
     }
 
-    private fun saveCategoria() {
-        val categoriaNombre = binding.spCategoria.selectedItem.toString()
+
+    private fun saveGastos() {
+        val categoriaNombre = categoriaGastosAdapter.getCategoriaSelected()?.nombre
         val categoriaMonto = binding.etMonto.text.toString().toFloatOrNull()
         val date = zonedDateTime.format(DateTimeFormatter.ofPattern("dd-MM-yyyy"))
-        val time = zonedDateTime.format(DateTimeFormatter.ofPattern("HH:mm:ss"))
-        val userId = FirebaseAuth.getInstance().currentUser?.uid
 
-        if (userId != null && categoriaNombre.isNotEmpty() && categoriaMonto != null) {
-            val categoriaId = database.child("Gastos").child(userId).push().key
-            if (categoriaId != null) {
-                val categoria = Categoria(categoriaNombre, categoriaMonto)
-                database.child("Gastos").child(userId).child(categoriaId).setValue(categoria)
-                    .addOnSuccessListener {
-                        Toast.makeText(
-                            context,
-                            "Categoría guardada exitosamente",
-                            Toast.LENGTH_SHORT
-                        ).show()
-                        binding.spCategoria.setSelection(0)
-                        binding.etMonto.text.clear()
-                        dismiss()
-                    }
-                    .addOnFailureListener {
-                        Toast.makeText(context, "Error al guardar la categoría", Toast.LENGTH_SHORT)
-                            .show()
-                    }
-            }
+        if (userId != null && categoriaNombre != null && categoriaMonto != null) {
+            val categoria = Categoria(categoriaNombre, categoriaMonto, date)
+            database.child("Gasto").child(userId).child((idCategoria + 1).toString())
+                .setValue(categoria)
+                .addOnSuccessListener {
+                    Toast.makeText(
+                        context,
+                        "Gasto guardado exitosamente",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                    binding.etMonto.text.clear()
+                    dismiss()
+                }
+                .addOnFailureListener {
+                    Toast.makeText(context, "Error al guardar el gasto", Toast.LENGTH_SHORT)
+                        .show()
+
+                }
         } else {
             Toast.makeText(context, "Por favor, complete todos los campos", Toast.LENGTH_SHORT)
                 .show()
@@ -97,20 +106,11 @@ class SheetGastos : BottomSheetDialogFragment() {
             if (dataSnapshot.exists()) {
                 for (ds: DataSnapshot in dataSnapshot.children) {
                     val categoriaNombre = ds.key
+                    val urlIcon = ds.child("urlicono").getValue(String::class.java)
 
-                    categoriaNombre?.let {
-                        arrayListCategoria.add(it)
-                    }
+                    arrayListCategoria.add(CategoriaGastos(categoriaNombre, urlIcon))
                 }
-
-                // Actualizar el Spinner con las categorías obtenidas
-                val adapter = ArrayAdapter(
-                    requireContext(),
-                    android.R.layout.simple_spinner_item,
-                    arrayListCategoria
-                )
-                adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-                binding.spCategoria.adapter = adapter
+                categoriaGastosAdapter.notifyDataSetChanged()
             }
         }.addOnFailureListener {
             Toast.makeText(context, "Algo salio mal", Toast.LENGTH_SHORT).show()
---
+++ app/src/main/java/com/example/finanzaspersonales/entidades/CategoriaGastos.kt
@@ -0,0 +1,6 @@
+package com.example.finanzaspersonales.entidades
+
+class CategoriaGastos(
+    val nombre: String? = null,
+    val urlImage: String? = null
+)
