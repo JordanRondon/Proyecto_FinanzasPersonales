97b7ffb50170f411b8539906a39d48d228a1588b
Diego Llaro <diegollarox@gmail.com>
2024-06-19 13:53:06 -0500

    Bold Budget Text in notifications

--- app/src/main/java/com/example/finanzaspersonales/adaptadores/NotificationAdapter.kt
+++ app/src/main/java/com/example/finanzaspersonales/adaptadores/NotificationAdapter.kt
@@ -1,5 +1,9 @@
 package com.example.finanzaspersonales.adaptadores
 
+import android.graphics.Typeface
+import android.text.SpannableString
+import android.text.Spanned
+import android.text.style.StyleSpan
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -29,11 +33,19 @@ class NotificationAdapter(private val notificationList: List<Notificacion>, priv
 
         fun render(notificationModel: Notificacion, onClickListener: (Notificacion)->Unit){
             binding.tvAsunto.text = notificationModel.asunto
-            binding.tvDescripcion.text = notificationModel.descripcion
+            binding.tvDescripcion.text = boldBudgetText(notificationModel.descripcion)
             binding.tvFecha.text = notificationModel.fecha
             binding.ivIcono.setImageResource(R.drawable.moneda)
             itemView.setOnClickListener{ onClickListener(notificationModel)}
         }
+    }
+    fun boldBudgetText(text : String): SpannableString{
+        //val text1 = "Se notifica que " + text.split("Se notifica que "," ha excedido")[1].uppercase() +" ha excedido el monto límite"
+        val length = text.split("Se notifica que ","ha excedido el monto límite")[1].length
+        val spannableString = SpannableString(text)
+        val boldSpan = StyleSpan(Typeface.BOLD)
+        spannableString.setSpan(boldSpan, 16, length +16, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+        return spannableString
     }
 
 }
